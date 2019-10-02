da156c1b9e21a04d72252a5fd72d6888fa3a82b6
unknown <simratpreetsingh.ss@gmail.com>
2019-10-02 17:04:31 +0530

    updated trie

--- Trie/Trie.cpp
+++ Trie/Trie.cpp
@@ -4,44 +4,68 @@ using namespace std;
 
 class TrieNode {
 public:
-	TrieNode * child[256];
+	TrieNode *child[26];
 	bool isEndOfWord;
-	TrieNode() {
-		for (int i = 0; i < 256; i++) {
+	TrieNode(){
+		for (int i = 0; i < 26; i++) {
 			child[i] = NULL;
 		}
 		isEndOfWord = false;
 	}
 };
 
-bool searchKey(TrieNode * root, string key) {
-	TrieNode * curr = root;
-	for (int i = 0; i < key.length(); i++) {
-		if (curr->child[key[i]]==NULL) {
-			curr = curr->child[key[i]];
+void addNode(TrieNode * root,string s) {
+	TrieNode* curr = root;
+	for (int i = 0; i < s.length(); i++) {
+		int index = s[i] - 'a';
+		if (!curr->child[index]) {
+			curr->child[index] = new TrieNode();
 		}
-	}
-	return curr->isEndOfWord;
-}
-
-void addNode(TrieNode * root,string key) {
-	TrieNode * curr = root;
-	for (int i = 0; i < key.length(); i++) {
-		if (curr->child[key[i]] == NULL) {
-			curr = new TrieNode();
-			curr = curr->child[key[i]];
-		}
+		curr = curr->child[index];
 	}
 	curr->isEndOfWord = true;
 }
 
-void deleteTrie(Node * root, string key) {
-
+bool search(TrieNode * root, string s) {
+	TrieNode * curr = root;
+	for (int i = 0; i < s.length(); i++) {
+		int index = s[i] - 'a';
+		if (!curr->child[index]) {
+			return false;
+		}
+		curr = curr->child[index];
+	}
+	return curr->isEndOfWord;
 }
 
+void printWord(char * str, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << str[i];
+	}
+	cout << endl;
+}
 
+void print(TrieNode * root,char str[],int level) {
+	if (root == NULL) {
+		return;
+	}
+	if (root->isEndOfWord) {
+		printWord(str, level);
+	}
+	for (int i = 0; i < 26; i++) {
+		if (root->child[i]) {
+			str[level] = (char)(i + 'a');
+			print(root->child[i], str, level + 1);
+		}
+	}
+}
 
 int main() {
-	// your code goes here
-	return 0;
+	TrieNode * root = new TrieNode();
+	addNode(root,"b");
+	addNode(root, "ab");
+	addNode(root, "bca");
+	int level = 0;
+	char str[20];
+	print(root, str, level);
 }
